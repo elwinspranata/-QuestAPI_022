0f98b657f6a6186a4db5f5defb7357a20d90a6ce
elwinspranata <elwinpranata02@gmail.com>
2024-12-29 23:04:10 +0700

    Menambahkan fungsi PengelolaHalaman untuk mengatur navigasi dengan NavHost

---
+++ app/src/main/java/com/example/praktikum12/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,40 @@
+package com.example.praktikum12.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum12.ui.ViewMahasiswa.DestinasiEntry
+import com.example.praktikum12.ui.ViewMahasiswa.DestinasiHome
+import com.example.praktikum12.ui.ViewMahasiswa.EntryMhsScreen
+import com.example.praktikum12.ui.ViewMahasiswa.HomeScreen
+
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToltemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = {
+                }
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    popUpTo(DestinasiHome.route){
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+
+}
