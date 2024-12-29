e7ce22b6426e9302ee6b2c125bc833504c496fc3
elwinspranata <elwinpranata02@gmail.com>
2024-12-30 01:34:42 +0700

    Menambahkan MahasiswaApplications untuk inisialisasi AppContainer

---
+++ app/src/main/java/com/example/praktikum12/ui/MahasiswaApplication.kt
@@ -0,0 +1,15 @@
+package com.example.praktikum12.ui
+
+import android.app.Application
+import com.example.praktikum12.di.AppContainer
+import com.example.praktikum12.di.MahasiswaContainer
+
+
+class MahasiswaApplications:Application() {
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        container = MahasiswaContainer()
+    }
+}
